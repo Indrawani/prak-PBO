c58a1398d5ff2a588e9479ac25ab9a877994a29e
Indrawani <waniindra58@gmail.com>
2022-02-19 17:09:15 +0700

    semoga bisa ngoding amin

---
+++ mainanalisisalgo.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+	int hasil=6;
+	int a=1,b=2,c=3;
+
+	if(a<b){
+		if(b<c){
+			hasil = 7;
+		}
+	}else{
+		hasil = 8;
+	}
+	cout<<hasil<<endl;
+}
